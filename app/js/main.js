7392d0c4f4dc70b0532e74cf578fa1e0b13f518d
VitaliyW <sham_xd@mail.ru>
2020-09-20 04:22:56 +0300

    slider-on and icons fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
-  
+  $('.products-slider__slider-inner').slick({
+    arrows: true,
+    dots: false,
+    prevArrow: '<button type="button" class="lnr-chevron-left"></button>',
+    nextArrow: '<button type="button" class="lnr-chevron-right"></button>'
+  });
+
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    starWidth: "14px",
+    readOnly: true,
+    spacing: "3px",
+    normalFill: "#fff",
+    ratedFill: "#ffc000"
+  });
+
 });
